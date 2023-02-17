5a55b7259930e96f596cfb4cc51605cacb52f52c
İbrahim EDİZ <edizdesign@gmail.com>
2023-02-17 14:41:16 +0000

    17022023_2

--- Exercises/MertKuttekin/01_02_str.py
+++ Exercises/MertKuttekin/01_02_str.py
@@ -2,5 +2,3 @@ isim = input("isminizi giriniz:")
 print(isim,type(isim))
 print("isminizin tersten yazimi:",isim[::-1])
 
-var1 ='Teşekkürler Süpermen'
-print(var1.replace("e","i").replace("ü","i"))
