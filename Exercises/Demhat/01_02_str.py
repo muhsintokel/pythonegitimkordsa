43498e17e8a107f1e0659b9a7006f92418237279
İbrahim EDİZ <edizdesign@gmail.com>
2023-02-17 13:11:01 +0000

    17022023_1

--- Exercises/Demhat/01_02_str.py
+++ Exercises/Demhat/01_02_str.py
@@ -6,8 +6,8 @@ print("isminizin ikinci harfi: ", isim[1])
 
 """
 #Ödev1
-var1 = "Teşekkürler.Süpermen"
-word1,word2 = var1.split(.)
+var1 = "Teşekkürler Süpermen"
+word1,word2 = var1.split()
 print(word1,word2.replace("e","i"))
 
 
