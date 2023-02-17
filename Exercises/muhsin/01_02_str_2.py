7755f8b30de7b77906b3861b9ed123d1358b7cb3
muhsintokel <125568998+muhsintokel@users.noreply.github.com>
2023-02-17 19:34:56 +0300

    Update 01_02_str_2.py

--- Exercises/muhsin/01_02_str_2.py
+++ Exercises/muhsin/01_02_str_2.py
@@ -4,3 +4,12 @@ a,b,c,d = var1.split(",") # => [41,5,59,N]
 #                               0  1  2  3
 # print(b)
 print(var1.split(",")[1])
+
+
+print("Metin değiştirme programına hoş geldiniz!")
+text = input("Metni girin: ")
+text = text.replace('a', '@')
+text = text.replace('i', '!')
+text = text.replace('o', '#')
+
+print("Değiştirilmiş metin: ", text)
